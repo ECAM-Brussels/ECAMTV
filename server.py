a87503bb4c9df7577d5fe1f90e655fea88c6a623
Sébastien Combéfis <seb478@gmail.com>
2016-06-07 10:46:20 +0200

    Server: Refactor modules loading

--- server.py
+++ server.py
@@ -3,6 +3,8 @@
 # Author: Tom Selleslagh
 # Version: June 7, 2016
 
+import importlib
+import inspect
 import json
 import os
 
@@ -22,36 +24,41 @@ def get_static_file(rep, filename):
 def get_module_asset(name, file):
     return static_file(file, root='./modules/{}'.format(name))
 
+# Load a list of modules
 def loadmodules(modules):
     assets = {'js': [], 'css': []}
+    widgets = {}
     for module in modules:
-        path = 'modules/{}'.format(module)
+        path = 'modules/{}'.format(module['name'])
         if os.path.isdir(path):
+            # Load CSS and JS files
             for entry in os.listdir(path):
                 (name, ext) = os.path.splitext(entry)
                 if ext == '.css':
                     assets['css'].append('{}/{}'.format(path, entry))
                 elif ext == '.js':
                     assets['js'].append('{}/{}'.format(path, entry))
-    return assets
+            # Load widgets
+            mod = importlib.import_module('modules.{0}.{0}'.format(module['name']))
+            for name, obj in inspect.getmembers(mod):
+                if inspect.isclass(obj) and name != 'Module':
+                    obj = obj(module['config']) if 'config' in module else obj()
+                    widgets[module['name']] = obj.widget()
+    return (assets, widgets)
 
 # Main page
 @route('/')
 def main():
-    assets = loadmodules(['datetime', 'weather', 'transport', 'warning', 'logo'])
-    import modules.datetime.datetime
-    d = modules.datetime.datetime.DateTime().widget()
-    import modules.weather.weather
-    we = modules.weather.weather.Weather().widget()
-    import modules.transport.transport
-    t = modules.transport.transport.Transport().widget()
-    import modules.warning.warning
-    wa = modules.warning.warning.Warning().widget()
-    import modules.logo.logo
-    l = modules.logo.logo.Logo({'src': 'images/ecam-logo.png', 'alt': 'Logo ECAM'}).widget()
+    assets, widgets = loadmodules([
+        {'name': 'datetime'},
+        {'name': 'weather'},
+        {'name': 'transport'},
+        {'name': 'warning'},
+        {'name': 'logo', 'config': {'src': 'images/ecam-logo.png', 'alt': 'Logo ECAM'}}
+    ])
     import modules.filler.filler
     lf = modules.filler.filler.Filler(height=100).widget()
-    return template('index.html', assets=assets, datetime=d(), weather=we(), transport=t(), warning=wa(), logo=l(), leftfiller=lf())
+    return template('index.html', assets=assets, widgets=widgets, leftfiller=lf())
 
 # Launch the server
 run(host=HOST, port=PORT, debug=True)
