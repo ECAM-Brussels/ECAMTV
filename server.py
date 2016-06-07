274b3704a4dd07a25c726abeaae33fe882044ef5
Sébastien Combéfis <seb478@gmail.com>
2016-06-07 07:29:04 +0200

    Server: Remove unnecessary routes

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Author: Sébastien Combéfis
 # Author: Tom Selleslagh
-# Version: May 28, 2016
+# Version: June 7, 2016
 
 import json
 import os
@@ -11,10 +11,6 @@ from bottle import *
 # Global configuration variables
 PORT = int(os.environ.get('PORT', 5000))
 HOST = '0.0.0.0'
-EVENT = "./database/event.ddb"
-STUDENT = "./database/students.ddb"
-HORAIRE = "./database/horaire.ddb"
-INFOS = "./database/infos.ddb"
 
 # Generic route for static files
 @route('/<rep:re:(css|js|images|files)>/<filename>')
@@ -55,34 +51,7 @@ def main():
     l = modules.logo.logo.Logo({'src': 'images/ecam-logo.png', 'alt': 'Logo ECAM'}).widget()
     import modules.filler.filler
     lf = modules.filler.filler.Filler(height=100).widget()
-    with open(EVENT, 'r') as dico:
-            dicoevent = json.load(dico)
-    return template('index.html', assets=assets, datetime=d(), weather=we(), transport=t(), warning=wa(), logo=l(), leftfiller=lf(), event=dicoevent)
+    return template('index.html', assets=assets, datetime=d(), weather=we(), transport=t(), warning=wa(), logo=l(), leftfiller=lf())
 
-#------ Planning Page -------
-@route('/planning')
-def main():
-    return template ('html/keypad.html')
-
-@route('/planning/<matricule>')
-def planning(matricule=14029):
-    with open(STUDENT, 'r') as stud:
-        allstudent = json.load(stud)
-        student = allstudent['inscrit'][matricule]
-    with open(HORAIRE, 'r') as hora:
-        horaire = json.load(hora)
-    return template('./html/planning.html',student=student,horaire=horaire)
-
-#------ Student Life Page -------
-@route('/info')
-def main():
-    with open(INFOS, 'r') as info:
-        listepdf = json.load(info)
-    return template('./html/infos.html',listing=listepdf)
-
-#------ underconstruct page-------
-@route('/underconstruction')
-def main():
-    return template('./html/underconstruct.html')
-
+# Launch the server
 run(host=HOST, port=PORT, debug=True)
